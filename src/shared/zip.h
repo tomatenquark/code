f087561ce873f8c9b9a6a66b410932a8c06527f4
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2020-03-26 15:32:31 +0100

    Export addzip function.

---
+++ src/shared/zip.h
@@ -0,0 +1,13 @@
+//
+//  zip.h
+//  tomatenquark
+//
+//  Created by Lennard Berger on 26.03.20.
+//
+
+#ifndef _ZIP_H
+#define _ZIP_H
+
+extern bool addzip(const char *name, const char *mount = NULL, const char *strip = NULL);
+
+#endif /* zip_h */
