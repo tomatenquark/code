b60093e0e69ef3e2425ef7688ab276fab017e0d6
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2020-03-30 21:13:37 +0200

    Revert "Also export renderprogress."
    This reverts commit 4bf04d785a8a20a35aa4b0a6d53c6ec7cf3276fa.

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -442,7 +442,7 @@ extern bool interceptkey(int sym);
 
 extern float loadprogress;
 extern void renderbackground(const char *caption = NULL, Texture *mapshot = NULL, const char *mapname = NULL, const char *mapinfo = NULL, bool restore = false, bool force = false);
-extern void renderprogress(float bar, const char *text, GLuint tex, bool background);
+extern void renderprogress(float bar, const char *text, GLuint tex = 0, bool background = false);
 
 extern void getfps(int &fps, int &bestdiff, int &worstdiff);
 extern void swapbuffers(bool overlay = true);
