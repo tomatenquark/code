5e8862af31ee5421fd052705de6defd66de5d163
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-03-31 03:35:38 +0000

    don't localconnect while initing
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@5810 07af1644-1164-4067-b897-63bf4dc9b748

--- src/engine/server.cpp
+++ src/engine/server.cpp
@@ -727,6 +727,7 @@ void localdisconnect(bool cleanup)
 
 void localconnect()
 {
+    if(initing) return;
     client &c = addclient(ST_LOCAL);
     copystring(c.hostname, "local");
     game::gameconnect(false);
