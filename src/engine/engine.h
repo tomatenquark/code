fc0c11078736c5e6f673d9cb139da7396e8c1015
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2020-04-01 22:25:06 +0200

    Add renderprogress to make downloads more user friendly.

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -441,7 +441,6 @@ extern void pushevent(const SDL_Event &e);
 extern bool interceptkey(int sym);
 
 extern float loadprogress;
-extern void renderprogress(float bar, const char *text, GLuint tex = 0, bool background = false);
 
 extern void getfps(int &fps, int &bestdiff, int &worstdiff);
 extern void swapbuffers(bool overlay = true);
