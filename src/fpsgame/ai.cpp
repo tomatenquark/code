0042282727cdac1582b01f1a43972b43f6dfb61a
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-04-21 23:42:42 +0000

    aidebug CON_DEBUG
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@5925 07af1644-1164-4067-b897-63bf4dc9b748

--- src/fpsgame/ai.cpp
+++ src/fpsgame/ai.cpp
@@ -148,7 +148,7 @@ namespace ai
         bool resetthisguy = false;
         if(!d->name[0])
         {
-            if(aidebug) conoutf("%s assigned to %s at skill %d", colorname(d, name), o ? colorname(o) : "?", sk);
+            if(aidebug) conoutf(CON_DEBUG, "%s assigned to %s at skill %d", colorname(d, name), o ? colorname(o) : "?", sk);
             else conoutf("\f0join:\f7 %s", colorname(d, name));
             resetthisguy = true;
         }
@@ -156,10 +156,10 @@ namespace ai
         {
             if(d->ownernum != ocn)
             {
-                if(aidebug) conoutf("%s reassigned to %s", colorname(d, name), o ? colorname(o) : "?");
+                if(aidebug) conoutf(CON_DEBUG, "%s reassigned to %s", colorname(d, name), o ? colorname(o) : "?");
                 resetthisguy = true;
             }
-            if(d->skill != sk && aidebug) conoutf("%s changed skill to %d", colorname(d, name), sk);
+            if(d->skill != sk && aidebug) conoutf(CON_DEBUG, "%s changed skill to %d", colorname(d, name), sk);
         }
 
         copystring(d->name, name, MAXNAMELEN+1);
