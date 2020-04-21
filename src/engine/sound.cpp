f1ef3e9ccccc651aa37f30090987d9d86cfb2ac1
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-04-21 23:40:48 +0000

    dbgsound CON_DEBUG
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@5924 07af1644-1164-4067-b897-63bf4dc9b748

--- src/engine/sound.cpp
+++ src/engine/sound.cpp
@@ -779,7 +779,7 @@ int playsound(int n, const vec *loc, extentity *ent, int flags, int loops, int f
     soundslot &slot = sounds.slots[config.chooseslot(flags)];
     if(!slot.sample->chunk && !slot.sample->load()) return -1;
 
-    if(dbgsound) conoutf("sound: %s", slot.sample->name);
+    if(dbgsound) conoutf(CON_DEBUG, "sound: %s", slot.sample->name);
  
     chanid = -1;
     loopv(channels) if(!channels[i].inuse) { chanid = i; break; }
@@ -813,7 +813,7 @@ void stopsounds()
 bool stopsound(int n, int chanid, int fade)
 {
     if(!gamesounds.configs.inrange(n) || !channels.inrange(chanid) || !channels[chanid].inuse || !gamesounds.playing(channels[chanid], gamesounds.configs[n])) return false;
-    if(dbgsound) conoutf("stopsound: %s", channels[chanid].slot->sample->name);
+    if(dbgsound) conoutf(CON_DEBUG, "stopsound: %s", channels[chanid].slot->sample->name);
     if(!fade || !Mix_FadeOutChannel(chanid, fade))
     {
         Mix_HaltChannel(chanid);
