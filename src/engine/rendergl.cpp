4bbc67577eb3dc5137ecebae328e975929df2e8c
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-05-25 23:27:24 +0200

    Allow floating-point values for `crosshairsize`
    This makes finer size adjustments possible.

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -2069,7 +2069,7 @@ void drawdamagescreen(int w, int h)
 VAR(hidestats, 0, 0, 1);
 VAR(hidehud, 0, 0, 1);
 
-VARP(crosshairsize, 0, 15, 50);
+FVARP(crosshairsize, 0, 15, 50);
 VARP(cursorsize, 0, 30, 50);
 VARP(crosshairfx, 0, 1, 1);
 VARP(crosshaircolors, 0, 1, 1);
