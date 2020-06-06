62df6ebd5ce19f3f6f8e4518f34d2393bf4d87ae
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-06-06 19:30:55 +0000

    clamp atmohaze
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@6014 07af1644-1164-4067-b897-63bf4dc9b748

--- src/engine/rendersky.cpp
+++ src/engine/rendersky.cpp
@@ -429,7 +429,7 @@ static void drawatmosphere(int w, float z1clip = 0.0f, float z2clip = 1.0f, int 
     float planetradius = earthradius*atmoplanetsize, atmoradius = planetradius + earthatmoheight*atmoheight;
     LOCALPARAMF(opticaldepthoffset, (atmoradius*atmoradius) / (planetradius*planetradius) - 1);
 
-    float gm = (1 - atmohaze)*0.2f + 0.75f;
+    float gm = clamp(0.95f - 0.2f*atmohaze, 0.0f, 1.0f);
     LOCALPARAMF(mie, 1 + gm*gm, -2*gm);
 
     static const vec lambda(680e-9f, 550e-9f, 450e-9f),
