cf4fb13aba5a1bef92747995549b5c8386ea0f59
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-06-27 04:58:19 +0000

    lightcompress normal fix
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@6061 07af1644-1164-4067-b897-63bf4dc9b748

--- src/engine/lightmap.cpp
+++ src/engine/lightmap.cpp
@@ -416,7 +416,7 @@ static inline bool htcmp(const lightmapinfo &k, const layoutinfo &v)
         vcolor += kbpp*LM_PACKW;
     }
     if((ktype&LM_TYPE) != LM_BUMPMAP0) return true;
-    const bvec *kdir = k.raybuf, *vdir = (const bvec *)lightmaps[v.lmid+1 - LMID_RESERVED].data;
+    const bvec *kdir = k.raybuf, *vdir = (const bvec *)lightmaps[v.lmid+1 - LMID_RESERVED].data + (v.x + v.y*LM_PACKW);
     for(int i = 0; i < int(kh); i++)
     {
         if(memcmp(kdir, vdir, kw*sizeof(bvec))) return false;
