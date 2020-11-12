52154bbf41feefeee510ed2689c9d955f6d2ab42
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-11-12 17:13:57 +0000

    X11 pointer grab workaround
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@6239 07af1644-1164-4067-b897-63bf4dc9b748

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -2,6 +2,10 @@
 
 #include "engine.h"
 
+#ifdef SDL_VIDEO_DRIVER_X11
+#include "SDL_syswm.h"
+#endif
+
 extern void cleargamma();
 
 void cleanup()
@@ -471,6 +475,17 @@ void inputgrab(bool on, bool delay = false)
             {
                 SDL_SetWindowGrab(screen, SDL_TRUE);
                 relativemouse = true;
+
+                #ifdef SDL_VIDEO_DRIVER_X11
+                // Workaround for buggy SDL X11 pointer grabbing
+                union { SDL_SysWMinfo info; uchar buf[sizeof(SDL_SysWMinfo) + 512]; };
+                SDL_GetVersion(&info.version);
+                if(SDL_GetWindowWMInfo(screen, &info) && info.subsystem == SDL_SYSWM_X11)
+                {
+                    uint mask = ButtonPressMask | ButtonReleaseMask | PointerMotionMask | FocusChangeMask;
+                    XGrabPointer(info.info.x11.display, info.info.x11.window, True, mask, GrabModeAsync, GrabModeAsync, info.info.x11.window, None, CurrentTime);
+                }
+                #endif
             }
             else
             {
